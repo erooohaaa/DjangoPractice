c3d4e02c1318c5c5e2de8eb3859cb7142f0618e4
Yerkanat <erkanatm249@gmail.com>
2025-12-04 15:11:28 +0500

    last

--- practice1/main/views.py
+++ practice1/main/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'title': 'Главная страница'})
 def about(request):
     return render(request, 'main/about.html')
